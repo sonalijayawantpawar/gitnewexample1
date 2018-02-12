ea225a50ce9b978c33f9a3aa895b3b5591262f77
sonali <sonali34p@gmail.com>
2018-02-12 15:38:16 +0530

    Add project files.

---
+++ gitnewexample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitnewexample.Startup))]
+namespace gitnewexample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
